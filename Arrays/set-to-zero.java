6efb2571a181fbef0ae84977903345951c66d53a
Varun Bhardwaj <varun.bhardwaj@stl.tech>
2023-07-19 19:18:58 +0530

    Add array set-to-zero optimal

--- Arrays/set-to-zero.java
+++ Arrays/set-to-zero.java
@@ -1,17 +1,57 @@
-//https://takeuforward.org/data-structure/set-matrix-zero/
-//https://www.codingninjas.com/studio/problems/zero-matrix_1171153
-
-import java.util.*;
-import java.io.*;
-
+import java.util.* ;
+import java.io.*; 
 public class Solution {
     public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
-        
-        
-        //return bruteForce(matrix, n, m);
-        return betterApproach(matrix,n, m);
-    }
+        // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
+       //return bruteForce(matrix, n, m) ;
+       //return betterApproach(matrix,n, m);
+       return optimalApproach(matrix,n,m);
 
+    }
+    public static ArrayList<ArrayList<Integer>> optimalApproach(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+        int col0 = 1;
+        // step 1: Traverse the matrix and
+        // mark 1st row & col accordingly:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark i-th row:
+                    matrix.get(i).set(0, 0);
+
+                    // mark j-th column:
+                    if (j != 0)
+                        matrix.get(0).set(j, 0);
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix.get(i).get(j) != 0) {
+                    // check for col & row:
+                    if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
+                        matrix.get(i).set(j, 0);
+                    }
+                }
+            }
+        }
+
+        //step 3: Finally mark the 1st col & then 1st row:
+        if (matrix.get(0).get(0) == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix.get(0).set(j, 0);
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix.get(i).set(0, 0);
+            }
+        }
+
+        return matrix;
+    }
     public static ArrayList<ArrayList<Integer>> betterApproach(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
 
         int[] row = new int[n]; 
@@ -36,10 +76,9 @@ public class Solution {
         
 
         return matrix;
-    }    
-    public static ArrayList<ArrayList<Integer>> bruteForce(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
-        // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
-        for (int i = 0; i < n; i++) {
+    }
+    public static ArrayList<ArrayList<Integer>> bruteForce(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m){
+for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix.get(i).get(j) == 0) {
                     markRow(matrix, n, m, i);
@@ -56,9 +95,8 @@ public class Solution {
             }
         }
         return matrix;
-
+       
     }
-
     static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i) {
         // set all non-zero elements as -1 in the row i:
         for (int j = 0; j < m; j++) {
@@ -76,5 +114,5 @@ public class Solution {
             }
         }
     }
-
+   
 }
