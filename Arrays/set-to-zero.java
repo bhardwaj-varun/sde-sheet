54a306f07d84296f016483798baf43dbba547973
Varun Bhardwaj <varun.bhardwaj@stl.tech>
2023-07-19 18:46:40 +0530

    Add array set-to-zero bruteforce

---
+++ Arrays/set-to-zero.java
@@ -0,0 +1,52 @@
+//https://www.codingninjas.com/studio/problems/zero-matrix_1171153
+
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+        
+        return bruteForce(matrix, n, m);
+    }
+
+    public static ArrayList<ArrayList<Integer>> bruteForce(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+        // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    markRow(matrix, n, m, i);
+                    markCol(matrix, n, m, j);
+                }
+            }
+        }
+        // Finally, mark all -1 as 0:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == -1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+        return matrix;
+
+    }
+
+    static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i) {
+        // set all non-zero elements as -1 in the row i:
+        for (int j = 0; j < m; j++) {
+            if (matrix.get(i).get(j) != 0) {
+                matrix.get(i).set(j, -1);
+            }
+        }
+    }
+
+    static void markCol(ArrayList<ArrayList<Integer>> matrix, int n, int m, int j) {
+        // set all non-zero elements as -1 in the col j:
+        for (int i = 0; i < n; i++) {
+            if (matrix.get(i).get(j) != 0) {
+                matrix.get(i).set(j, -1);
+            }
+        }
+    }
+
+}
